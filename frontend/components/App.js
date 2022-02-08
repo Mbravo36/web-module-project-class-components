780561482be50311a463ff7e52757fb4dc0159be
Your Name <mari@Maribels-MacBook-Pro.local>
2022-02-07 20:26:22 -0600

    built state

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,16 +1,39 @@
 import React from 'react'
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          name: 'Organize Garage',
+          id: 1528817077286, // could look different, you could use a timestamp to generate it
+          completed: false
+        },
+        {
+          name: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+      ]
+    }
+  }
+  
+  
   render() {
+    const {todos} = this.state;
+    console.log(todos);
     return (
       <div>
         <h1>Todo App</h1>
         <ul>
-          <li>Walk the dog</li>
-          <li>Take out trash</li>
-          <li>WorkOut</li>
+          {
+            todos.map(todo=> {
+              return (<li>{todo.name} {todo.completed?<span>-completed</span> : <span></span> }</li>)
+            })
+          }
         </ul>
-        
+
           <form>
             <input/>
             <button>Add</button>
