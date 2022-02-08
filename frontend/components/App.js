14707dd8b3e99681fe59cbad0b1a75997f7da224
Your Name <mari@Maribels-MacBook-Pro.local>
2022-02-07 18:24:35 -0600

    added dom

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,7 +4,20 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        Todo App
+        <h1>Todo App</h1>
+        <ul>
+          <li>Walk the dog</li>
+          <li>Take out trash</li>
+          <li>WorkOut</li>
+        </ul>
+        
+          <form>
+            <input/>
+            <button>Add</button>
+          </form>
+
+          <button>Clear</button>
+        
       </div>
     )
   }
